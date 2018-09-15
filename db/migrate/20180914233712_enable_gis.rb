5bad7c0b52de93634b2f6338cbdee8a57f4ad639
Barry Nagel <barry.nagel@bcgdv.com>
2018-09-15 12:22:47 +0200

    Fixes gis setup

--- db/migrate/20180914233712_enable_gis.rb
+++ db/migrate/20180914233712_enable_gis.rb
@@ -1,6 +1,5 @@
 class EnableGis < ActiveRecord::Migration[5.2]
   def change
     enable_extension 'postgis'
-    enable_extension 'postgis_topology'
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,40 +16,11 @@ ActiveRecord::Schema.define(version: 2018_09_14_233747) do
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
   enable_extension "postgis"
-  enable_extension "postgis_topology"
   enable_extension "uuid-ossp"
-
-  create_table "layer", primary_key: ["topology_id", "layer_id"], force: :cascade do |t|
-    t.integer "topology_id", null: false
-    t.integer "layer_id", null: false
-    t.string "schema_name", null: false
-    t.string "table_name", null: false
-    t.string "feature_column", null: false
-    t.integer "feature_type", null: false
-    t.integer "level", default: 0, null: false
-    t.integer "child_id"
-    t.index ["schema_name", "table_name", "feature_column"], name: "layer_schema_name_table_name_feature_column_key", unique: true
-  end
 
   create_table "lists", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "spatial_ref_sys", primary_key: "srid", id: :integer, default: nil, force: :cascade do |t|
-    t.string "auth_name", limit: 256
-    t.integer "auth_srid"
-    t.string "srtext", limit: 2048
-    t.string "proj4text", limit: 2048
-  end
-
-  create_table "topology", id: :serial, force: :cascade do |t|
-    t.string "name", null: false
-    t.integer "srid", null: false
-    t.float "precision", null: false
-    t.boolean "hasz", default: false, null: false
-    t.index ["name"], name: "topology_name_key", unique: true
-  end
-
-  add_foreign_key "layer", "topology", name: "layer_topology_id_fkey"
 end
